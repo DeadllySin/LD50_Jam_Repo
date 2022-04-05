0cddb342dc3465435e93018edf1d915800537eaf
DeadllySin <52604400+DeadllySin@users.noreply.github.com>
2022-04-05 16:48:30 +0100

    Added test sound (need to update ref in Scene)

--- LD50/Assets/Scripts/AudioManager.cs
+++ LD50/Assets/Scripts/AudioManager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager am;
+
+    [Header("Player")]
+    public FMODUnity.EventReference testSoundEvent;
+
+
+    void Awake()
+    {
+        if (am != null)
+        {
+            Destroy(this);
+            Debug.Log("AudioManager not loaded");
+        }    
+    am = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKeyDown("t"))
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(testSoundEvent);
+        }
+
     }
 }
