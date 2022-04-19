715808c9ab61607072602d8463b51523c9022387
DeadllySin <52604400+DeadllySin@users.noreply.github.com>
2022-04-19 13:56:12 +0100

    Tests for raytrace

--- LD50/Assets/Scripts/Audio/A_CeilingTrace.cs
+++ LD50/Assets/Scripts/Audio/A_CeilingTrace.cs
@@ -8,34 +8,35 @@ public class A_CeilingTrace : MonoBehaviour
     [SerializeField] bool isSeeingCeiling;
     [SerializeField] float rayDistance = 20f;
     [SerializeField] float fov = 90f;
+    [SerializeField] Transform EyesTest;
+    GameObject ceiling;
 
     private void Start()
     {
-        
+        ceiling = GameObject.FindGameObjectWithTag("Ceiling");
     }
 
     private void Update()
     {
 
-        isSeeingCeiling = A_Trace_Utils.CanYouSeeThis(transform.parent, GameManager.gm.ceiling.transform, null, fov, rayDistance);
-        Debug.DrawRay(transform.parent.position, transform.forward * rayDistance, isSeeingCeiling ? Color.green : Color.red, 2.0f);
-
+        Debug.DrawRay(transform.position, transform.forward * rayDistance, isSeeingCeiling ? Color.green : Color.red, 2.0f);
+        
         RaycastHit hit;
-        if (Physics.Raycast(transform.parent.position, transform.forward, out hit, rayDistance))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, rayDistance))
         {
             if (hit.collider.gameObject.tag == "Ceiling")
             {
+                Debug.Log("true");
                 //isSeeingCeiling = true;
-
+                //Debug.Log(GameManager.gm.ceiling.transform);
             }
             //Debug.Log(hit.collider.name);
             
-            if (hit.collider.name != "Player")
-            {
-                Debug.Log(hit.collider.name);
-            }
         }
         
+        Debug.Log(GameManager.gm.ceiling.transform);
+        //isSeeingCeiling = A_Trace_Utils.CanYouSeeThis(transform, GameManager.gm.ceiling.transform, null, fov, rayDistance);
+        isSeeingCeiling = A_Trace_Utils.CanYouSeeThis(EyesTest, ceiling.transform, "Ceiling", fov, rayDistance);
         if (isSeeingCeiling)
         {
             Debug.Log("I can YES see the ceiling");
