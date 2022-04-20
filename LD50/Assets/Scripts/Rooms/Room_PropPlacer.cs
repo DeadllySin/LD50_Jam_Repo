64079514eb460639bf7b44044c21bdc9c4e45c1e
DeadllySin <52604400+DeadllySin@users.noreply.github.com>
2022-04-20 16:43:26 +0100

    Random prop placer draft

---
+++ LD50/Assets/Scripts/Rooms/Room_PropPlacer.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room_PropPlacer : MonoBehaviour
+{
+    [SerializeField] private GameObject[] propMeshess;
+    readonly List <GameObject> spawners = new List<GameObject>();
+    readonly List<GameObject> propMeshes = new List<GameObject>();
+    [SerializeField] private GameObject spawnerParent;
+    private Main_Room room;
+
+    private void Awake()
+    {
+        room = GetComponentInParent<Main_Room>();
+        for (int i = 0; i < propMeshess.Length; i++) propMeshes.Add(propMeshess[i]);
+        foreach (Transform child in spawnerParent.transform) spawners.Add(child.gameObject);
+    }
+
+    private void Start()
+    {
+        while (propMeshes.Count > 0)
+        {
+            int temp = Random.Range(0,spawners.Count - 1);
+            int temp2 = Random.Range(0, propMeshes.Count - 1);
+            Instantiate(propMeshes[temp2], spawners[temp].transform.position, Quaternion.identity);
+            spawners.RemoveAt(temp);
+            propMeshes.RemoveAt(temp2);
+        }
+    }
+
+    private void Update()
+    {
+        if (room)
+        {
+            Debug.Log("Room_PropPlacer: ");
+        }
+    }
+}
