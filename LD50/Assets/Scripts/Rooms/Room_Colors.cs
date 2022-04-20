0628966a82dcccb1f131770f8d3f939212010be5
IbrahimOezhan <ibrahimoezhan1207@gmail.com>
2022-04-20 14:53:23 +0200

    pushed some updates

--- LD50/Assets/Scripts/Rooms/Room_Colors.cs
+++ LD50/Assets/Scripts/Rooms/Room_Colors.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 public class Room_Colors : MonoBehaviour
 {
-    [SerializeField] private GameObject[] spawners;
+    [SerializeField] private Vector3[] spawners;
     [SerializeField] private GameObject[] colorPrefab;
     private List <string> colorOrder;
-    private List<GameObject> spawnersList = new List<GameObject>();
     private List<GameObject> colorList = new List<GameObject>();
     private int pressedButtons;
     private bool[] correctPresses = new bool[5];
 
     private void Start()
     {
-        for(int i = 0; i < spawners.Length; i++)
-        {
-            spawnersList.Add(spawners[i]);
-        }
-
         for(int i = 0; i < colorPrefab.Length; i++)
         {
             colorList.Add(colorPrefab[i]);
@@ -28,12 +22,9 @@ public class Room_Colors : MonoBehaviour
         while (colorList.Count > 0) 
         {
 
-            int temp = Random.Range(0, spawnersList.Count - 1);
             int temp2 = Random.Range(0, colorList.Count - 1);
-            GameObject color = Instantiate(colorList[temp2], spawners[temp].transform.position, Quaternion.identity);
-            spawners[temp] = color;
+            GameObject color = Instantiate(colorList[temp2], spawners[j], Quaternion.identity);
             colorOrder.Add(color.GetComponent<Interactable_ColorOrder>().color);
-            spawnersList.RemoveAt(temp);
             colorList.RemoveAt(temp2);
             j++;
         }
@@ -41,6 +32,7 @@ public class Room_Colors : MonoBehaviour
 
     public void onPressed(string color)
     {
+        Debug.Log("color" + color + "presed");
         if(color == colorOrder[pressedButtons])
         {
             correctPresses[pressedButtons] = true;
