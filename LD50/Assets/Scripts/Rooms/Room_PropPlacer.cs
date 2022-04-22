68e1d26c6e1aefcc9ec19895e5a7a045ec21ef41
IbrahimOezhan <ibrahimoezhan1207@gmail.com>
2022-04-22 18:32:55 +0200

    Update Room_PropPlacer.cs

--- LD50/Assets/Scripts/Rooms/Room_PropPlacer.cs
+++ LD50/Assets/Scripts/Rooms/Room_PropPlacer.cs
@@ -3,41 +3,31 @@ using UnityEngine;
 
 public class Room_PropPlacer : MonoBehaviour
 {
-    [SerializeField] private GameObject[] propMeshess;
     readonly List<GameObject> spawners = new List<GameObject>();
     readonly List<GameObject> propMeshes = new List<GameObject>();
+    [SerializeField] private GameObject[] propMeshess;
     [SerializeField] private GameObject spawnerParent;
-    private Room_Main room;
+    [SerializeField] private int spawnCount = 0;
 
     private void Awake()
     {
-        room = GetComponentInParent<Room_Main>();
         for (int i = 0; i < propMeshess.Length; i++) propMeshes.Add(propMeshess[i]);
         foreach (Transform child in spawnerParent.transform) spawners.Add(child.gameObject);
-
-        Debug.Log(spawners.Count);
     }
 
     private void Start()
     {
-        while (propMeshes.Count > 0)
+        if(spawnCount < propMeshes.Count)
         {
-            int spawnersIndex = Random.Range(0, spawners.Count);
-            int propIndex = Random.Range(0, propMeshes.Count);
-            Instantiate(propMeshes[propIndex], spawners[spawnersIndex].transform.position, Quaternion.identity);
-            spawners.RemoveAt(spawnersIndex);
-            propMeshes.RemoveAt(propIndex);
-            
-            //Debug.Log("temp " + spawnersIndex);
-            //Debug.Log("temp2 " + propIndex);
+            for (int i = 0; i < spawnCount; i++)
+            {
+                int spawnersIndex = Random.Range(0, spawners.Count);
+                int propIndex = Random.Range(0, propMeshes.Count);
+                Instantiate(propMeshes[propIndex], spawners[spawnersIndex].transform.position, Quaternion.identity);
+                spawners.RemoveAt(spawnersIndex);
+                propMeshes.RemoveAt(propIndex);
+            }
         }
-    }
 
-    private void Update()
-    {
-        if (room)
-        {
-            //Debug.Log("Room_PropPlacer: ");
-        }
     }
 }
