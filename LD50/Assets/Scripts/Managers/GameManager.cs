3a55a9993c2275f9a9f3f5542018e919613cdefc
IbrahimOezhan <ibrahimoezhan1207@gmail.com>
2022-05-12 15:35:13 +0200

    added timer. fixed bugs

--- LD50/Assets/Scripts/Managers/GameManager.cs
+++ LD50/Assets/Scripts/Managers/GameManager.cs
@@ -8,7 +8,8 @@ public class GameManager : MonoBehaviour
     private bool isDead;
     public GameObject player;
     public GameObject room;
-    public Light l;
+    [SerializeField] private Light l;
+    private bool doOnce;
     [HideInInspector] public GameObject currRoom;
     [HideInInspector] public string currRoomType;
     [HideInInspector] public Tunnel currTunnel;
@@ -42,17 +43,13 @@ public class GameManager : MonoBehaviour
     [HideInInspector] public float ceilingSpeed;
     [HideInInspector] public GameObject ceilingSourceChild;
 
-    private float startTime;
-    private string minutes;
-    private string seconds;
-
-
     private void Awake()
     {
         gm = this;
+        GameState.gs.introFinished = GameState.gs.skipCutscene;
         testOutDeath = GameState.gs.killFast;
         Cursor.lockState = CursorLockMode.Locked;
-        mainMenu.SetActive(GameState.gs.introFinished);
+        mainMenu.SetActive(!GameState.gs.introFinished);
         playerCine.SetActive(GameState.gs.skipCutscene);
         cutsceneCam.SetActive(!GameState.gs.skipCutscene);
         cutscene.SetActive(!GameState.gs.skipCutscene);
@@ -60,7 +57,6 @@ public class GameManager : MonoBehaviour
         player.SetActive(GameState.gs.skipCutscene);
         if (testOutDeath) defaultSpeed = 2; slowThresholdSpeed = 2;
         //defaultSpeed = ceilingSpeedScale(defaultSpeed, 0f, 10f, 0f, 0.5f);
-        startTime = Time.time;
         ceilingSpeed = defaultSpeed;
         currRoom = FindObjectOfType<Room_Main>().gameObject;
         currTunnel = FindObjectOfType<Tunnel>();
@@ -84,9 +80,15 @@ public class GameManager : MonoBehaviour
 
         if (GameState.gs.introFinished == true) //|| AudioManager.am.GetComponent<A_MusicCallBack>().FMODIntroDoOnce == true)
         {
+            if (!doOnce)
+            {
+                doOnce = true;
+                dustStorm.SetActive(false);
+                l.gameObject.SetActive(false);
+                Timer.timer.countTime = true;
+            }
             Debug.Log("ceiling is moving");
-            dustStorm.SetActive(false);
-            l.gameObject.SetActive(false);
+
             ceiling.transform.position = Vector3.MoveTowards(ceiling.transform.position, new Vector3(ceiling.transform.position.x, ceiling.transform.position.y - 7, ceiling.transform.position.z), ceilingSpeed * Time.deltaTime);
             //Fmod stuff
             ceilingSourceChild.transform.position = new Vector3(player.transform.position.x, ceiling.transform.position.y - 0.5f, player.transform.position.z);
@@ -106,33 +108,19 @@ public class GameManager : MonoBehaviour
     {
         //Debug control variables
         //Debug.Log("intro finished " + GameState.gs.introFinished + "|| skip cutscene " + GameState.gs.skipCutscene + "|| fmod restart" + AudioManager.am.FMODRestarted);
-
-        /*//test pause menu when skip cutscene active
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            mainMenu.SetActive(false);
-        }*/
         
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            Pause();
-        }
+        if (Input.GetKeyDown(KeyCode.Tab)) Pause();
     }
 
     private void OnDeath()
     {
         isDead = true;
+        Timer.timer.countTime = false;
         if (roomsCleared > PlayerPrefs.GetInt("roomsCleared")) PlayerPrefs.SetInt("roomsCleared", roomsCleared);
-        scoreText.text = "you cleared " + roomsCleared + " rooms!\n your highscore is " + PlayerPrefs.GetInt("roomsCleared") + "\npress r to restart\npress m to go to the main menu\npress esc to quit";
+        scoreText.text = "you cleared " + roomsCleared + " rooms!\n your highscore is " + PlayerPrefs.GetInt("roomsCleared") + "\nyou survived for " + Timer.timer.time +  "\npress r to restart\npress m to go to the main menu\npress esc to quit";
         deathScreen.SetActive(true);
         player.SetActive(false);
         AudioManager.am.FMOD_DeadState();
-
-        //timer
-        float t = Time.time - startTime;
-        minutes = ((int)t / 60).ToString();
-        seconds = (t % 60).ToString("f0");
-        Debug.Log(minutes + " mins" + seconds + " secs");
     }
 
     public void Pause()
@@ -143,5 +131,4 @@ public class GameManager : MonoBehaviour
         if(!player.activeInHierarchy) AudioManager.am.pauseSSInstance.start();
         else AudioManager.am.pauseSSInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
     }
-
 }
